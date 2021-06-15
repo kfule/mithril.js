5c10c1e81e991e5fefa08602fa47114f0b80692b
kfule <kta@live.jp>
2021-06-15 19:59:10 +0900

    null check for function component

--- render/render.js
+++ render/render.js
@@ -150,7 +150,7 @@ module.exports = function($window) {
 			if (sentinel.$$reentrantLock$$ != null) return
 			sentinel.$$reentrantLock$$ = true
 			vnode.state = (vnode.tag.prototype != null && typeof vnode.tag.prototype.view === "function") ? new vnode.tag(vnode) : vnode.tag(vnode)
-			if (typeof vnode.state.view !== "function" && typeof vnode.tag === "function") {
+			if (vnode.state != null && typeof vnode.state.view !== "function" && typeof vnode.tag === "function") {
 				vnode.state = { view: vnode.tag }
 			}
 		}
