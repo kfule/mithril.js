08a9e4ece389752a9a61990340b170000938398c
Keita Furui <kta@live.jp>
2023-02-04 10:05:48 +0900

    build without m.request, m.route

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 "use strict"
 
 var m = require("./hyperscript")
-var request = require("./request")
 var mountRedraw = require("./mount-redraw")
 
 m.Fragment = "["
 m.mount = mountRedraw.mount
-m.route = require("./route")
 m.render = require("./render")
 m.redraw = mountRedraw.redraw
-m.request = request.request
-m.parseQueryString = require("./querystring/parse")
-m.buildQueryString = require("./querystring/build")
-m.parsePathname = require("./pathname/parse")
-m.buildPathname = require("./pathname/build")
 m.vnode = require("./render/vnode")
 m.censor = require("./util/censor")
 
